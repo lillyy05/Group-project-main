eaacc79861810325876ced8e32647db60bb06b03
lillyy05 <B00165167@mytudublin.ie>
2024-12-10 21:46:38 +0000

    Update PlayerController.cs

--- Group-project-main/Assets/Scripts/PlayerController.cs
+++ Group-project-main/Assets/Scripts/PlayerController.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 15;
+    public float speed = 15f;
     public float horizontalInput;
-    
+
+    public GameManager gameManager; // Reference to GameManager for scoring
+
+    private Rigidbody playerRb; // Reference to Rigidbody
+
     // Start is called before the first frame update
     void Start()
     {
-      
+        playerRb = GetComponent<Rigidbody>(); // Initialize Rigidbody
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Making him move forward
+        // Move forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
+        // Horizontal movement
         horizontalInput = Input.GetAxis("Horizontal");
-        //Moving him left and right
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+    
+
+        // Handle collision with obstacles
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Dog collided with an obstacle!");
+            Destroy(gameObject); // Destroy the dog
+            gameManager.GameOver(); // Trigger game over logic
+        }
     }
 }
